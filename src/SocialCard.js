dcd4e8f671512ca717d44dc4450e65918ade1a18
e1900303 <tranduyphat080300@gmail.com>
2022-03-02 22:00:04 +0200

    add some css and profile pc

--- src/SocialCard.js
+++ src/SocialCard.js
@@ -1,7 +1,12 @@
+import './SocialCard.css'
+
 const SocialCard = ({ userData }) => {
     return (
-        <div>
-            {userData.name.first}
+        <div className="card">
+            <div className="card_title">{userData.name.first} {userData.name.last}</div>
+            <div className="card_body">
+                <div className="card__image"><img src={userData.picture.medium}/></div>
+            </div>
         </div>
     )
 }
