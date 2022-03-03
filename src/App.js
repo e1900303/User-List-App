f7d2ab07c2569cd084610b20be8bbf6bd8104a3e
e1900303 <tranduyphat080300@gmail.com>
2022-03-03 16:53:30 +0200

    finished product

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SocialCard from './SocialCard'
 function App() {
   
   const [users, setUser] = useState([])
+  const [allUsers, setAllUsers] = useState([])
 
   useEffect(() => {
     (async () => {
@@ -17,18 +18,33 @@ function App() {
         console.log(error)
         userData = []
       }
+      setAllUsers(userData)
       setUser(userData)
     })()
   }, [])
   
-  
+  const filterCards = event => {
+    const value = event.target.value.toLowerCase()
+    const filteredUsers = allUsers.filter(
+      user => (`${user.name.first} ${user.name.last}`
+      .toLowerCase()
+      .includes(value)
+      )
+    )
+    
+    setUser(filteredUsers)
+  }
   
   
   return (
     <div className='App'>
+      <h1>Social Cards</h1>
+      <input className='search-box' placeholder='Search...' onInput={filterCards}/>
+      <div className='cards-container'>
       {users.map((user, index) => (
         <SocialCard userData={user} key={index}/>
       ))}
+      </div>
     </div>
   )
 }
--- src/SocialCard.js
+++ src/SocialCard.js
@@ -1,11 +1,16 @@
 import './SocialCard.css'
+import Location from './Location'
+import PhoneNumber from './PhoneNumber'
 
 const SocialCard = ({ userData }) => {
     return (
         <div className="card">
             <div className="card_title">{userData.name.first} {userData.name.last}</div>
             <div className="card_body">
-                <div className="card__image"><img src={userData.picture.medium}/></div>
+                <Location location={userData.location}/>
+                <PhoneNumber type="Home" number={userData.phone}></PhoneNumber>
+                <PhoneNumber type="Mobile" number={userData.cell}></PhoneNumber>
+                <div className="card_image"><img src={userData.picture.medium}/></div>
             </div>
         </div>
     )
